a1953631e994368434e1723f6b17dbc163f016ab
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2024-12-07 06:10:44 +0300

    submit changes all

--- app.py
+++ app.py
@@ -83,10 +83,8 @@ topics = st.sidebar.text_area("Enter topics (e.g., Java, SpringBoot, MySQL)", pl
 start_interview = st.sidebar.button("Start Interview")
 
 # Ensure session state for form handling
-#if 'submitted' not in st.session_state:
-#    st.session_state['submitted'] = False
-#if 'user_answers' not in st.session_state:
-#    st.session_state['user_answers'] = {}
+if 'submitted' not in st.session_state:
+    st.session_state['submitted'] = False
 
 # Ensure session state for questions and user answers
 if 'questions' not in st.session_state:
@@ -95,38 +93,35 @@ if 'user_answers' not in st.session_state:
     st.session_state['user_answers'] = {}
 if 'ai_answers' not in st.session_state:
     st.session_state['ai_answers'] = []
+
+
 # Main Page
 # When user clicks on the "Start Interview" button
 isEmpty = not username or not job_title or not experience_level or not topics
 if start_interview and not isEmpty:
-    st.write(f"Hello, {username}! Let's start your interview for a {experience_level} level {job_title } position. Topics selected: {topics}")
-    st.write("Generating your interview questions...")
-   
     # Generate questions and store them in session state
     questions, ai_answers = get_gemini_response(topics)
     st.session_state['questions'] = questions
     st.session_state['user_answers'] = {}
     st.session_state['ai_answers'] = ai_answers
+    st.session_state['submitted'] = False
 
+    st.write(f"Hello, {username}! Let's start your interview for a {experience_level} level {job_title } position. Topics selected: {topics}")
+    st.write("Generating your interview questions...")
 elif start_interview and isEmpty:
     st.warning('Please fill in all the fields to start the interview.', icon="⚠️")
 
 print("if else dışında.")
 print("Questions in state: ", st.session_state['questions'])
-# Post-submission actions
-#if st.session_state['submitted']:
-#    st.write("Form submitted successfully! 🚀")
-#    st.write("Here are your answers:")
-#    for i, (question, answer) in enumerate(st.session_state['user_answers'].items(), start=1):
-#        st.subheader(f"Question {i}: {question}")
-#        st.write(f"Your Answer: {answer}")
 
 # Display questions and form if questions exist
-if st.session_state['questions']:
+if st.session_state['questions'] and not st.session_state['submitted']:
+    print("Ques and not submitter")
     form = st.form(key='questions_form')
     form.write("Please provide your answers to the questions below.")
     user_answers = st.session_state['user_answers']
     ai_answers = st.session_state['ai_answers']
+
     for i, question in enumerate(st.session_state['questions']):
         form.write(f"Question {i + 1}: {question}")
         user_answers[question] = form.text_input(label="Answer", 
@@ -140,6 +135,7 @@ if st.session_state['questions']:
     print("user_answers in form:", user_answers)
 
     if submitted:
+        st.session_state['submitted'] = True
         st.write("Form submitted successfully! 🚀")
         st.write("Here are your answers:")
         ai_answers = st.session_state['ai_answers']
@@ -151,8 +147,11 @@ if st.session_state['questions']:
             st.write(f"AI Answer: {ai_answers[i-1]}")
             st.write(f"Score: {user_score[i-1]}")
         
-        submitted = False
-
+        st.session_state['questions'] = []
+        st.session_state['user_answers'] = {}
+        st.session_state['ai_answers'] = []
+        st.session_state['submitted'] = False
+    
 prompt = "Create interview questions at most 3."
 model = genai.GenerativeModel(model_name="gemini-1.5-flash-lates")
 #user_prompt=st.text_input("Please enter the job title you are applying for", key='message')
